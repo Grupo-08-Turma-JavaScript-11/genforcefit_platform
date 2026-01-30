b3eebc953f71ad1ada7f04e8add70526d218a8eb
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2026-01-30 19:07:03 -0300

    feat: adiciona rotas de usuario no app

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
 import Login from "./pages/login/Login"
+import CadastroUsuario from "./pages/cadastro/CadastroUsuario"
+import ListarUsuarios from "./components/usuario/listarusuarios/ListarUsuarios"
+import DeletarUsuario from "./components/usuario/deletarusuario/DeletarUsuario"
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* Login */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Cadastro de usuário */}
+        <Route path="/cadastro" element={<CadastroUsuario />} />
+
+        {/* Listagem (listarcaronas) */}
+        <Route path="/listarcaronas" element={<ListarUsuarios />} />
+
+        {/* Deletar usuário */}
+        <Route path="/deletarusuario/:id" element={<DeletarUsuario />} />
       </Routes>
     </BrowserRouter>
   )
