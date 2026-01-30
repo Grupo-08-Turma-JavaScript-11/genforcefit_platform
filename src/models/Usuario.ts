c3162c1113a0280cf3aceddac295edd237d7251e
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2026-01-30 18:29:03 -0300

    feat: cria models de usuario e login

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,7 +1,12 @@
 export default interface Usuario {
+  id?: number
   nome: string
-  usuario: string
-  senha: string
-  tipo: string
-  foto: string
+  usuario: string // email
+  senha?: string
+  tipo: "Aluno"
+  altura?: number
+  peso?: number
+  IMC?: number
+  foto?: string
+  exercicio?: string[]
 }
